8eb35a6871287cbac035dcd2946f6c57e6903185
Burak Mestan <burakmestan@gmail.com>
2019-02-02 20:11:52 +0300

    serving static js and css with easy setup

---
+++ products/static/js/index.js
@@ -0,0 +1,8 @@
+const name = document.getElementById('name')
+const counter = document.getElementById('counter')
+
+let count = 0
+
+name.innerText = 'Burak'
+
+setInterval(() => counter.innerText = ++count, 1000)
